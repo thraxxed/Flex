67fda83204bb336d4b98ccbb4d4c18c0a7882338
seth <sealittl@ucsc.edu>
2018-02-16 13:49:43 -0800

    add gender and age to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216210337) do
+ActiveRecord::Schema.define(version: 20180216214732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20180216210337) do
     t.string "bio", null: false
     t.float "latitude", null: false
     t.float "longitude", null: false
+    t.integer "age", null: false
+    t.string "gender", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
