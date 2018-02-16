33d9c622d3067982565b18ddbb00d30c289203f2
seth <sealittl@ucsc.edu>
2018-02-16 13:42:23 -0800

    update paperclip gem to 5.2.0

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  match_id   :integer          not null
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
