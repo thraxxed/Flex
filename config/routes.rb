dbca3a35401ab4fb2b310897611564c270260b43
Zach Smith <zzachsmith@gmail.com>
2018-02-16 14:39:44 -0800

    add session controller + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api , defaults: { format: :json} do
+    resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :show, :update]
   end
 end
