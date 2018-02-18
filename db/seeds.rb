b29122076c423de62f3b4d8bba5531d78b4c0ca2
seth <sealittl@ucsc.edu>
2018-02-17 20:04:22 -0800

    seed 1 meme into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+m = Meme.create(image_url: "https://i.redd.it/hrzhyf8irtg01.png")
