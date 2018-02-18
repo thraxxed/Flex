33656c6e90fd8358cb0a6e4184f53acf0f3b7e67
seth <sealittl@ucsc.edu>
2018-02-17 22:08:25 -0800

    destroy all memes at beginning of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Meme.destroy_all
 
 m = Meme.create(image_url: "https://i.redd.it/hrzhyf8irtg01.png")
 m2 = Meme.create(image_url: "https://i.redd.it/bcijtq2h6vg01.jpg")
