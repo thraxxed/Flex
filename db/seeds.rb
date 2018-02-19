fae97e1ea75b0d51ef3460927cab141109f80c9c
seth <sealittl@ucsc.edu>
2018-02-18 21:24:45 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Meme.destroy_all
+User.destroy_all
 
 m = Meme.create(image_url: "https://i.redd.it/hrzhyf8irtg01.png")
 m2 = Meme.create(image_url: "https://i.redd.it/bcijtq2h6vg01.jpg")
-m3 = Meme.create(image_url: "https://isitfunnyoroffensive.com/wp-content/uploads/2016/08/Arthur-adult-memes.png")
-m4 = Meme.create(image_url: "https://img.memecdn.com/arthur-meme_o_7206167.jpg")
+m3 = Meme.create(image_url: "https://i.redditmedia.com/siWQE_7inDqiPOZYNTnls_VQJWrzm0X2wek7SEOr1qo.jpg?w=1024&s=abed5b483fc327a8373d5c7f111bca94")
+m4 = Meme.create(image_url: "https://i.redditmedia.com/csDnXZ554OB68moHf8Dv_XUNZRlmfu2LyvVdScM07Nw.jpg?w=530&s=d9fea163df6611abbfadf28b68f1d286")
