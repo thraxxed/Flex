eb3f7e2e9e343965aa537a1d336b9e355422896c
seth <sealittl@ucsc.edu>
2018-02-19 19:31:04 -0800

    aws s3 region stuff

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,11 +6,10 @@ class Api::UsersController < ApplicationController
     @user = User.new(username: params[:username], password: params[:password],
                      latitude: params[:latitude], longitude: params[:longitude])
     @user.bio = ""
-    # @user.latitude = 420
-    # @user.longitude = 420
+
     @user.age = 18
     @user.gender = "F"
-    # @user.picture = File.open('http://www.dkvine.com/games/dkc/characters/images/diddy_kong_05.png')
+
     if @user.save
       login(@user)
       render :show
@@ -24,16 +23,12 @@ class Api::UsersController < ApplicationController
   end
 
   def update
-    # p params
     @user = User.find(params[:id])
     user_picture = params[:picture]
     user_picture_file = File.open('user_picture.png', 'wb') do|f|
       f.write(Base64.decode64(user_picture))
     end
-    p "hey"
-    p user_picture_file
     @user.picture = File.open('user_picture.png')
-    # if @user.update(picture: user_picture_file)
     if @user.save
       render :show
     else
