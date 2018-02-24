d689da0fd85f853549d29125b857fbb18b36a4af
seth <sealittl@ucsc.edu>
2018-02-24 14:32:23 -0800

    messages controller #index

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Match.find(params[:id]).messages
+    match = Match.find_by(user1_id: params[:id], user2_id: current_user.id)
+    unless match
+      match = Match.find_by(user2_id: params[:id], user1_id: current_user.id)
+    end
+    @messages = match.messages
   end
 end
