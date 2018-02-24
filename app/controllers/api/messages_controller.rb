03bd509a8faa3676c74b3608be8d31016d9bd5fc
Zach Smith <zzachsmith@gmail.com>
2018-02-24 15:04:23 -0800

    add create method to messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -6,4 +6,19 @@ class Api::MessagesController < ApplicationController
     end
     @messages = match.messages
   end
+
+  def create
+    @message = Message.new(body: params[:body])
+    match = Match.find_by(user1_id: params[:id], user2_id: current_user.id)
+    unless match
+      match = Match.find_by(user2_id: params[:id], user1_id: current_user.id)
+    end
+    @message.match = match
+    @message.user = current_user
+    if @message.save
+      render json: 'hey'
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
 end
